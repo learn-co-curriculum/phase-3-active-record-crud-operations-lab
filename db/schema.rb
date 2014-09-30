904494db74538fa687fe4eb390531dc804a8cd53
Ian Miller <irmiller22@gmail.com>
2014-09-30 15:26:18 -0400

    created schema, migration file

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 20140930192253) do
 
-  create_table "movies", force: :cascade do |t|
-    t.string "title"
-    t.datetime "release_date"
-    t.string "director"
-    t.string "lead"
+  create_table "movies", force: true do |t|
+    t.string  "title"
+    t.integer "release_date"
+    t.string  "director"
+    t.string  "lead"
     t.boolean "in_theaters"
   end
 
