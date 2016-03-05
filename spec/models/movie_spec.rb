6b3616cb74f10be00d55ee441044848d73c4f6c2
Brian Holland <bhollan@users.noreply.github.com>
2016-03-04 22:53:04 -0600

    Correct a spec's call to a function
    `can_find_using_where_clause` was missing `_and_be_sorted`.

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -110,7 +110,7 @@ describe 'Movie' do
       end
 
       it 'can use a where clause and be sorted' do
-        expect(can_find_using_where_clause.map{|m| m.title}).to eq(["Movie_4", "Movie_3"])
+        expect(can_find_using_where_clause_and_be_sorted.map{|m| m.title}).to eq(["Movie_4", "Movie_3"])
       end
     end
 
