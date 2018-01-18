e5aec1a249a2216dfe0cf56dc2f6fa4ac3cde17d
Johann Kerr <johannkerr@gmail.com>
2018-01-18 10:26:24 -0500

    initial commit

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,11 @@
 require 'bundler/setup'
 Bundler.require
 
+
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  :database  => "db/#{ENV["SINATRA_ENV"]}"
 )
+
 
 require_all 'app'
