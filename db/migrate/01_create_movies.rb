52e0cebe5110358d51fc14220f134a5132ffcdf7
Matt Lawford <mlawford7@gmail.com>
2018-02-20 16:06:00 -0500

    updated solution

--- db/migrate/01_create_movies.rb
+++ db/migrate/01_create_movies.rb
@@ -1,11 +1,11 @@
 class CreateMovies < ActiveRecord::Migration[4.2]
   def change
     create_table :movies do |t|
-      t.string   :title
-      t.integer  :release_date
-      t.string   :director
-      t.string   :lead
-      t.boolean  :in_theaters
+      t.string :title
+      t.datetime :release_date
+      t.string :director
+      t.string :lead
+      t.boolean :in_theaters
     end
   end
 end
