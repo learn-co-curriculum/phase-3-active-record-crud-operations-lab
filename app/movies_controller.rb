40a1591f2f5fdf8ea314d64639988bcfee96601f
Matt Lawford <mlawford7@gmail.com>
2018-02-20 16:06:00 -0500

    updated solution

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -11,14 +11,24 @@ def can_be_instantiated_and_then_saved
   movie.save
 end
 
-def can_be_created_with_a_hash_of_attributes(attributes)
-  movie = Movie.create(attributes)
+def can_be_created_with_a_hash_of_attributes
+  attributes = {
+      title: "The Sting",
+      release_date: 1973,
+      director: "George Roy Hill",
+      lead: "Paul Newman",
+      in_theaters: false
+  }
+  movie = Movie.new(attributes)
+  movie.save
+  movie
 end
 
-def can_be_created_in_a_block(title, year)
-  movie = Movie.create do |m|
-    m.title = title
-    m.release_date = year
+def can_be_created_in_a_block
+  Movie.create do |m|
+    m.title = "Home Alone"
+    m.release_date = 1990
+    m.save
   end
 end
 
@@ -31,32 +41,33 @@ def can_get_the_last_item_in_the_database
 end
 
 def can_get_size_of_the_database
-  Movie.all.size
+  Movie.count
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.find(1).title
+  Movie.find_by(id:1).title
 end
 
 def can_find_by_multiple_attributes
   # title == "Title"
-  # release_date == 2000, 
+  # release_date == 2000
   # director == "Me"
-  Movie.find_by(title: "Title", release_date: 2000)
+  Movie.find_by(title: "Title", release_date: 2000, director: "Me")
 end
 
 def can_find_using_where_clause_and_be_sorted
-  # For this test return all movies released after 2002 and ordered by 
+  # For this test return all movies released after 2002 and ordered by
   # release date descending
   Movie.where("release_date > 2002").order(release_date: :desc)
 end
 
 def can_be_found_updated_and_saved
   # Update the title "Awesome Flick" to "Even Awesomer Flick"
-  Movie.create(title: "Awesome Flick")
-  movie = Movie.find_by(title: "Awesome Flick")
-  movie.title = "Even Awesomer Flick"
+  movie = Movie.create(title: "Awesome Flick")
+  movie = Movie.find_by(title:"Awesome Flick")
+  movie.update(title: "Even Awesomer Flick")
   movie.save
+
 end
 
 def can_update_using_update_method
@@ -71,18 +82,19 @@ def can_update_multiple_items_at_once
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  Movie.update_all(title: "A Movie")
+  Movie.all.each{|movie|
+  movie.update(title: "A Movie")}
 end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  movie = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
-  movie.destroy
+  movie= Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
+  movie.delete
 end
 
 def can_destroy_all_items_at_once
   10.times do |i|
     Movie.create(title: "Movie_#{i}")
   end
-  Movie.destroy_all
+  Movie.delete_all
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140930192253) do
+ActiveRecord::Schema.define(version: 1) do
 
   create_table "movies", force: :cascade do |t|
-    t.string  "title"
-    t.integer "release_date"
-    t.string  "director"
-    t.string  "lead"
+    t.string "title"
+    t.datetime "release_date"
+    t.string "director"
+    t.string "lead"
     t.boolean "in_theaters"
   end
 
