fcba1763856eddc7f2ab31a3373ada897806940d
curiositypaths <curiositypaths@users.noreply.github.com>
2018-03-22 13:53:13 -0400

    Update starter code to allow test execution

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,7 +22,7 @@ def can_be_created_with_a_hash_of_attributes
   movie = __
 end
 
-def can_be_created_in_a_block(args = {__})
+def can_be_created_in_a_block(args = __)
   Movie.create do |m|
     __
   end
