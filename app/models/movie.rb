8b3d20004be4d2122728b5390c792417aa5f37eb
OtienohOwinoh <coowinoh@gmail.com>
2022-11-06 20:53:53 +0300

    commit tree 1a6144e5e33c11f6f26203aa40f586907d4d5240

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,47 @@
 class Movie < ActiveRecord::Base
 
+    def self.create_with_title(title)
+        Movie.create(title: 'This is a title.')
+      end
+  
+      def self.first_movie
+        Movie.first
+      end
+  
+      def self.last_movie
+        Movie.last
+      end
+  
+      def self.movie_count
+        Movie.count
+      end
+  
+      def self.find_movie_with_id(id)
+        Movie.find(id)
+      end
+  
+      def self.find_movie_with_attributes(attributes)
+        Movie.find_by(attributes)
+      end
+  
+      def self.find_movies_after_2002
+        Movie.where("release_date > 2002")
+      end
+  
+      def update_with_attributes(attributes)
+        self.update(attributes)
+      end
+  
+      def self.update_all_titles(title)
+        Movie.update_all(title: title)
+      end
+  
+      def self.delete_by_id(id)
+        destroy(id)
+      end
+  
+      def self.delete_all_movies
+        Movie.destroy_all
+      end
+
 end
