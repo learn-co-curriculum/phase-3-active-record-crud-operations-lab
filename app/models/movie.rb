01c972bae10caec4d3772f8a80c4c60d5624c4d1
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-06-02 14:06:43 +0300

    Complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,46 +1,57 @@
 class Movie < ActiveRecord::Base
+
     def self.create_with_title(title)
         movie = self.new(title: title)
         movie.save
         movie
-      end
+    end
 
-      def self.first_movie
-        self.first
-      end
-    
-      def self.last_movie
-        self.last
-      end
-      
-      def self.movie_count
-        self.count
-      end
-    
-      def self.find_movie_with_id(id)
-        self.find(id)
-      end
-    
-      def self.find_movie_with_attributes(attributes)
-        self.find_by(attributes)
-      end
-    
-      def self.find_movies_after_2002
-        self.where("release_date > 2002")
-      end
 
-      def update_with_attributes(attributes)
-        self.update(attributes)
-      end
+    def self.first_movie
+    self.first
+    end
+
+
+    def self.last_movie
+    self.last
+    end
     
-      def self.update_all_titles(title)
-        self.update_all(title: title)
-      end
+
+    def self.movie_count
+    self.count
+    end
+
+
+    def self.find_movie_with_id(id)
+    self.find(id)
+    end
+
+
+    def self.find_movie_with_attributes(attributes)
+    self.find_by(attributes)
+    end
+
+
+    def self.find_movies_after_2002
+    self.where("release_date > 2002")
+    end
+
+
+    def update_with_attributes(attributes)
+    self.update(attributes)
+    end
+
+
+    def self.update_all_titles(title)
+    self.update_all(title: title)
+    end
+
 
     def self.delete_by_id(id)
         movie = self.find_by(id: id)
         movie.destroy if movie
     end
+    
     
     def self.delete_all_movies
         self.destroy_all
